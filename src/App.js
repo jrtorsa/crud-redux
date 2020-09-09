4999b5e3f48f362990209b6027aa7c5053447a67
Raul T <jr.torsa@gmail.com>
2020-09-09 15:20:33 -0600

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Fragment>
-      <h1>Redux</h1>
+      <Header />
     </Fragment>
   );
 }
